bddf35cf6d6eb149fa1b54622b74b6d5dc1a246a
Joao Bett <joao.gouveia.bettencourt@gmail.com>
2024-02-27 13:08:01 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
+
 public class HelloWorld extends JFrame {
     private JPanel panel;
+    private JLabel lblMensagem2;
 
     public HelloWorld() {
+        panel = new JPanel();
+        lblMensagem2 = new JLabel();
+        panel.add(lblMensagem2);
+        lblMensagem2.setText("O João Bett esteve aqui");
         setContentPane(panel);
         pack();
     }
